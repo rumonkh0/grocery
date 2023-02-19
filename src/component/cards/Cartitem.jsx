2cf372af21bd782fd380639bf27b56aa385b1e55
RUMON <rummankh0@gmail.com>
2023-02-19 14:39:55 +0600

    cart

--- src/component/cards/Cartitem.jsx
+++ src/component/cards/Cartitem.jsx
@@ -1,19 +1,31 @@
 import React from "react";
 import style from "./cartitem.module.css";
 import imag from "../../images/hexisol.webp";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMinus, faPlus, faXmark } from "@fortawesome/free-solid-svg-icons";
 
 function Cartitem() {
   return (
-    <div className="item">
+    <div className={style.item}>
       <div className={style.image}>
-        <img src={imag} alt="" />
+        <img src={imag} alt="Item" />
       </div>
       <div className={style.detail}>
-        <div className={style.name}></div>
-        <div className={style.quantity}></div>
-        <div className={style.price}></div>
+        <div className={style.name}>Hexisol Hand Rub</div>
+        <div className={style.quantity}>
+          <button>
+            <FontAwesomeIcon icon={faMinus} />{" "}
+          </button>
+          <div className={style.quancount}>2</div>
+          <button>
+            <FontAwesomeIcon icon={faPlus} />
+          </button>
+        </div>
+        <div className={style.price}>500 TK</div>
       </div>
-      <div className={style.remove}></div>
+      <div className={style.remove}>
+        <FontAwesomeIcon icon={faXmark} />
+      </div>
     </div>
   );
 }
