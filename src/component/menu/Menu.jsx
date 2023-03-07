b6472415475c8c0a15f62dfb7e5d2e1a0d5bb139
RUMON <rummankh0@gmail.com>
2023-03-07 17:00:43 +0600

    update

--- src/component/menu/Menu.jsx
+++ src/component/menu/Menu.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import style from "./menu.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
@@ -14,11 +14,10 @@ import {
   faKitchenSet,
   faCaretDown,
 } from "@fortawesome/free-solid-svg-icons";
-import { Link } from "react-router-dom";
+import { Link, useMatch, useResolvedPath } from "react-router-dom";
 
 function Menu({ menu }) {
   const [collapse, setCollapse] = useState(false);
-  const path = window.location.pathname;
 
   var toggle = () => {
     setCollapse(!collapse);
@@ -29,7 +28,15 @@ function Menu({ menu }) {
       <ul>
         <Li icon_name={faHouseChimney} href={"/"} text={"Home"} />
         <Li icon_name={faGift} href={"/"} text={"Special offer"} />
-        <Li icon_name={faBowlFood} href={"/signup"} text={"Food"} />
+        <li onClick={toggle}>
+          <FontAwesomeIcon className={style.icon} icon={faBowlFood} />
+          Food
+          <FontAwesomeIcon className={style.drop} icon={faCaretDown} />
+        </li>
+        <ul className={style.nested + " " + (collapse ? "" : style.collapse)}>
+          <Li href={"/"} text={"Fruits & Vegetable"} />
+          <Li href={"/"} text={"Breakfast"} />
+        </ul>
         <Li icon_name={faDumbbell} href={"/"} text={"Beautiful & health"} />
         <Li icon_name={faBaby} href={"/"} text={"Baby Care"} />
         <Li icon_name={faPumpMedical} href={"/"} text={"Hygiene"} />
@@ -37,30 +44,23 @@ function Menu({ menu }) {
         <Li icon_name={faCapsules} href={"/"} text={"Medicine"} />
         <Li icon_name={faBriefcase} href={"/"} text={"Office Product"} />
         <Li icon_name={faKitchenSet} href={"/"} text={"Kitchen Applience"} />
-
-        <li onClick={toggle}>
-          <FontAwesomeIcon className={style.icon} icon={faBowlFood} />
-          Food
-          <FontAwesomeIcon className={style.drop} icon={faCaretDown} />
-        </li>
-        <ul className={style.nested + " " + (collapse ? "" : style.collapse)}>
-          <li>Fruits & Vegetable</li>
-          <li>Breakfast</li>
-        </ul>
       </ul>
     </div>
   );
 }
 
 function Li({ icon_name, href, text, children, ...props }) {
-  const path = window.location.pathname;
-  useEffect(() => {
-  }, [path])
-  
+  const resolved = useResolvedPath(href);
+  const ismatch = useMatch({ path: resolved.pathname, end: true });
+  console.log(resolved);
   return (
     <Link to={href} {...props}>
-      <li className={href === path ? `${style.active}` : `${style.nactive}`}>
-        <FontAwesomeIcon className={style.icon} icon={icon_name} />
+      <li className={ismatch ? `${style.active}` : `${style.nactive}`}>
+        {icon_name ? (
+          <FontAwesomeIcon className={style.icon} icon={icon_name} />
+        ) : (
+          ""
+        )}
         {text}
         {children}
       </li>
